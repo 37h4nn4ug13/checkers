8bb38deeecdb2e7590c7a055a64f2979374415ea
37h4nn4ug13 <nnaugle1@vols.utk.edu>
2022-12-02 14:43:05 -0500

    Making the board look good

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "board.h" 
+using namespace std;
 
 int main() {
     Board board = Board(8);
     board.printBoard();
+
+    cout << "Wecome to Checkers. " << endl;
+    int turn = 0;
+    while(true) {
+        if (turn % 2 == 0) {
+            cout << "Player 1, Enter a move: ";
+            break;
+        }
+        
+    }
+    
 }
