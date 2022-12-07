f8e575609404e064d3c006abb43b169fac61d20e
37h4nn4ug13 <nnaugle1@vols.utk.edu>
2022-12-07 17:03:58 -0500

    Adding user input and adding to the move function

--- board.cpp
+++ board.cpp
@@ -1,4 +1,5 @@
 #include "board.h" 
+#include  <iomanip>
 
 Board::Board(const int &size) {
     Board::size = size;
@@ -26,10 +27,12 @@ Board::Board(const int &size) {
 }
 
 void Board::printBoard() {
+    int index = 0;
     for (int i = 0; i < Board::size; i++) {
         
         for (int j = 0; j < Board::size; j++) {
-            std::cout << "┏━━━┓";
+            printf("┏━%d%s┓", index, index >= 10 ? "" : "━");
+            index++;
         }
         std::cout << std::endl;
         for (int j = 0; j < Board::size; j++) {
@@ -42,33 +45,44 @@ void Board::printBoard() {
         }
         std::cout << std::endl;
     }
-    
+    // (void)index;
 }
 bool Board::move(const int &start, const int &end, const char &player) { // Will return false if move is invalid. 
     // Check is start is valid
-    if (((start / Board::size)%2==0 && start % 2 == 1) || ((start / Board::size) % 2 == 1 && start % 2 == 0)) {
+    if (!(((start / Board::size)%2==0 && start % 2 == 1) || ((start / Board::size) % 2 == 1 && start % 2 == 0))) {
         return false;
     }
     
-    // Check if end is valid
-    if (((end / Board::size)%2==0 && end % 2 == 1) || ((end / Board::size) % 2 == 1 && end % 2 == 0)) {
+    
+    // // Check if end is valid
+    if (!(((end / Board::size)%2==0 && end % 2 == 1) || ((end / Board::size) % 2 == 1 && end % 2 == 0))) {
+        return false;
+    }
+    // make sure you don't land on another piece
+    if (Board::spaces.at(end).getSymbol() != Checker(0).getSymbol()) {
         return false;
     }
 
-    // Check if user can move the piece
+    // // Check if user can move the piece
     if (Board::spaces.at(start).getSymbol() != player) {
         return false;
     }
 
+    
+
     if (player == 'X') {
-
-        if (end < start && Board::spaces.at(start).checkIsCrowned()) {
-            std::cout << std::endl;
+        if (!Board::spaces.at(start).checkIsCrowned()) {
+            if (start > end) {
+                return false;
+            }
         }
-
     }
     else if (player == 'O') {
-
+        if (!Board::spaces.at(start).checkIsCrowned()) {
+            if (end > start) {
+                return false;
+            }
+        }
     } 
     else {
         std::cerr << "Invalid function input. Player value of " << player <<  " is invalid. " << std::endl;
@@ -83,10 +97,21 @@ bool Board::move(const int &start, const int &end, const char &player) { // Will
         // make sure pieces being jumped are not the players pieces
     
     // Check for doubles
-
-    Board::spaces.at(end) = Board::spaces.at(start);
-    Board::spaces.at(start) = Checker(0);
-    return true;
+    if (
+        ((end == start + Board::size + 1 || end == start + Board::size - 1) || (start == end + Board::size + 1 || start == end + Board::size - 1)) ||
+        ((end == start + 2 * Board::size + 2 || end == start + 2 * Board::size - 2) || (start == end + 2 * Board::size + 2 || start == end + 2 * Board::size - 2))
+        ) { 
+            if (((end - start) > Board::size + 1) || ((start - end) > Board::size + 1)) {
+                // Condition for checking if there is a checker of the opposite player between start and end;
+                
+            }
+        // only allows pieces the move as far as they should. This also needs the jump logic. 
+        std::cout << player << std::endl;
+        Board::spaces.at(end) = Board::spaces.at(start);
+        Board::spaces.at(start) = Checker(0);
+        return true;
+    }
+    return false;
 }
 
 /*
--- main.cpp
+++ main.cpp
@@ -7,12 +7,57 @@ int main() {
 
     cout << "Wecome to Checkers. " << endl;
     int turn = 0;
+    int start, end;
+    char player;
+    bool isValid;
     while(true) {
+        isValid = false;
         if (turn % 2 == 0) {
-            cout << "Player 1, Enter a move: ";
-            break;
+            player = 'X';
         }
+        else {
+            player = 'O';
+        }
+        cout << "Player " << player << ", Enter a start index: ";
+            cin >> start;
+            cout << "Now enter an end index: ";
+            cin >> end;
+        while (!isValid) {
+            
+            isValid = board.move(start, end, player);
+            if (!isValid) {
+                cout << "invalid move" << endl;
+            }
+            else {
+                break;
+            }
+            cout << "Player " << player << ", Enter a start index: ";
+            cin >> start;
+            cout << "Now enter an end index: ";
+            cin >> end;
+        }
+        cout << "Printing new board" << endl;
+        board.printBoard();
+        turn ++;
         
     }
     
 }
+
+/*
+00 01 02 03 04 05 06 07
+
+08 09 10 11 12 13 14 15
+
+16 17 18 19 20 21 22 23
+
+24 25 26 27 28 29 30 31
+
+32 33 34 35 36 37 38 39
+
+40 41 42 43 44 45 46 47
+
+48 49 50 51 52 53 54 55
+
+56 57 58 59 60 61 62 63
+*/
